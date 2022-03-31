71474a1075f8433be44d13b9ac9813ef43cd3656
Thomas.Ulseth <FeGdPYxLKVkcXyMbyMix>
2022-03-31 22:58:02 +0200

    Add (styles and html for calculator)

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # jsCalculator
+ * From https://www.youtube.com/watch?v=j59qQ7YWLxw&ab_channel=WebDevSimplified
---
+++ script.js
@@ -0,0 +1,51 @@
+class Calculator {
+    constructor(previousOperationTextElement, currentOperationTextElement) {
+        this.previousOperationTextElement = previousOperationTextElement
+        this.currentOperationTextElement = currentOperationTextElement
+        this.clear()
+    }
+
+    clear() {
+        this.currentOperation = ''
+        this.previousOperation = ''
+        this.operation = undefined
+    }
+
+    delete() {
+
+    }
+
+    appendNumber(number) {
+        this.currentOperation = number
+    }
+
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+        this.currentOperationTextElement.innerText = this.currentOperation
+    }
+}
+
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperationTextElement = document.querySelector('[data-previous-operation]')
+const currentOperationTextElement = document.querySelector('[data-current-operation]')
+
+const calculator = new Calculator(previousOperationTextElement, currentOperationTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
